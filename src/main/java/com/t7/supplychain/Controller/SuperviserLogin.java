a8be5be58e8629cb342488e5f7a8161f3768fa02
varun-kajarekar <kajrekarvarun@gmail.com>
2024-02-29 20:20:47 +0530

    new

--- src/main/java/com/t7/supplychain/Controller/SuperviserLogin.java
+++ src/main/java/com/t7/supplychain/Controller/SuperviserLogin.java
@@ -4,10 +4,12 @@ import com.t7.supplychain.Entity.ResponseEntity;
 import com.t7.supplychain.Entity.SuperviserEntity;
 import com.t7.supplychain.Service.SuperviseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin()
 @RestController
 public class SuperviserLogin {
 
