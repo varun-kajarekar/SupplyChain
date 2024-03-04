4f67c2a1cea604bc5f8c3e5cb300286cd4ecde79
varun-kajarekar <kajrekarvarun@gmail.com>
2024-03-04 23:20:13 +0530

    a

--- src/main/java/com/t7/supplychain/Controller/Inventory.java
+++ src/main/java/com/t7/supplychain/Controller/Inventory.java
@@ -21,24 +21,6 @@ public class Inventory {
     SuperviseService superviseService;
 
 
-
-    @GetMapping("/inventory")
-    public void InventoryData() throws IOException {
-        URL url = new URL("https://api.thingspeak.com/channels/2449850/feeds/last.json?api_key=30VOJ2TIWYNRRKYE");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        int status = con.getResponseCode();
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-        //System.out.println(content.);
-    }
-
-
     @PostMapping("/add-requirement")
     public Requirement addrequirement(@RequestBody Requirement requirement){
 
